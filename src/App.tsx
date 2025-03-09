a8447e0e457d5d23bd10f6cb3a69b59e79ca8f55
giGa-1 <brb.giga.rom@gmail.com>
2025-03-10 00:07:59 +0100

    fix some maybe

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import PilotPresentation from "./components/PilotPresentation/PilotPresentation ";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PresentationScript from "./components/PresentationScript/PresentationScript";
+import EngineeringPresentation from "./components/PilotPresentation/PilotPresentation ";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<PilotPresentation />} />
+          <Route path="/" element={<EngineeringPresentation />} />
           <Route path="/content" element={<PresentationScript />} />
         </Routes>
       </BrowserRouter>
